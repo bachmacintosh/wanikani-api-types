f42063a1c6563dac0b245fb7b444d3a934939763
Collin Bachman <collin@bachman.io>
2022-10-28 19:08:06 -0400

    Use proper term for root exports
    Signed-off-by: Collin Bachman <collin@bachman.io>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /*
-This file exports all the types available in the recommended API version. Thereby, things MAY break in the future.
-Try sticking to imports from a specific version.
+This file exports all the types available in the recommended API revision. Thereby, things MAY break in the future.
+Try sticking to imports from a specific revision.
 */
 
 export { isWKDatableString, stringifyParameters } from "./v20170710.js";
