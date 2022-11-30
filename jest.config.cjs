f31eb3828122cb10ae0f29840f4fdae059cb78c3
Collin Bachman <collin@bachman.io>
2022-11-30 17:18:22 -0500

    Base: Check for Leap Years in isWKDatableString() (#74)
    * Base: Check for Leap Years in isWKDatableString()
    
    * Resolve ES Modules in Jest
    
    * Version bump
    
    Signed-off-by: Collin Bachman <collin@bachman.io>

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,4 +3,16 @@
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
+	extensionsToTreatAsEsm: [".ts"],
+	transform: {
+		"^.+\\.[tj]sx?$": [
+			"ts-jest",
+			{
+				useESM: true,
+			},
+		],
+	},
+	moduleNameMapper: {
+		"^(\\.{1,2}/.*)\\.js$": "$1",
+	},
 };
