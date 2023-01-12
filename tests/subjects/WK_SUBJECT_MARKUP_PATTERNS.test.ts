fea636edea7cbe41f273adefa970249eeaba6d85
Collin Bachman <collin@bachman.io>
2023-01-12 03:50:54 -0500

    More variable renames in tests
    Signed-off-by: Collin Bachman <collin@bachman.io>

--- tests/subjects/WK_SUBJECT_MARKUP_PATTERNS.test.ts
+++ tests/subjects/WK_SUBJECT_MARKUP_PATTERNS.test.ts
@@ -13,50 +13,50 @@ it("Matches Japanese text highlighting in <ja> tags", () => {
 
 it("Matches Kanji highlighting in <kanji> tags", () => {
 	const testString = `Two of WaniKani's Level 1 Kanji are <kanji>山</kanji> and <kanji>人</kanji>.`;
-	const matchedJa = [...testString.matchAll(WK_SUBJECT_MARKUP_MATCHERS.kanji)];
-	expect(matchedJa).toHaveLength(2);
-	expect(matchedJa[0][0]).toBe("<kanji>山</kanji>");
-	expect(matchedJa[0].groups?.innerText).toBe("山");
-	expect(matchedJa[1][0]).toBe("<kanji>人</kanji>");
-	expect(matchedJa[1].groups?.innerText).toBe("人");
+	const matchedText = [...testString.matchAll(WK_SUBJECT_MARKUP_MATCHERS.kanji)];
+	expect(matchedText).toHaveLength(2);
+	expect(matchedText[0][0]).toBe("<kanji>山</kanji>");
+	expect(matchedText[0].groups?.innerText).toBe("山");
+	expect(matchedText[1][0]).toBe("<kanji>人</kanji>");
+	expect(matchedText[1].groups?.innerText).toBe("人");
 });
 
 it("Matches Meaning highlighting in <meaning> tags", () => {
 	const testString = `The kanji 一 means <meaning>one</meaning>. The kanji 二 means <meaning>two</meaning>.`;
-	const matchedJa = [...testString.matchAll(WK_SUBJECT_MARKUP_MATCHERS.meaning)];
-	expect(matchedJa).toHaveLength(2);
-	expect(matchedJa[0][0]).toBe("<meaning>one</meaning>");
-	expect(matchedJa[0].groups?.innerText).toBe("one");
-	expect(matchedJa[1][0]).toBe("<meaning>two</meaning>");
-	expect(matchedJa[1].groups?.innerText).toBe("two");
+	const matchedText = [...testString.matchAll(WK_SUBJECT_MARKUP_MATCHERS.meaning)];
+	expect(matchedText).toHaveLength(2);
+	expect(matchedText[0][0]).toBe("<meaning>one</meaning>");
+	expect(matchedText[0].groups?.innerText).toBe("one");
+	expect(matchedText[1][0]).toBe("<meaning>two</meaning>");
+	expect(matchedText[1].groups?.innerText).toBe("two");
 });
 
 it("Matches Radical highlighting in <radical> tags", () => {
 	const testString = `One of the first radicals is the <radical>ground</radical> radical. A more complex one is <radical>coat rack</radical>.`;
-	const matchedJa = [...testString.matchAll(WK_SUBJECT_MARKUP_MATCHERS.radical)];
-	expect(matchedJa).toHaveLength(2);
-	expect(matchedJa[0][0]).toBe("<radical>ground</radical>");
-	expect(matchedJa[0].groups?.innerText).toBe("ground");
-	expect(matchedJa[1][0]).toBe("<radical>coat rack</radical>");
-	expect(matchedJa[1].groups?.innerText).toBe("coat rack");
+	const matchedText = [...testString.matchAll(WK_SUBJECT_MARKUP_MATCHERS.radical)];
+	expect(matchedText).toHaveLength(2);
+	expect(matchedText[0][0]).toBe("<radical>ground</radical>");
+	expect(matchedText[0].groups?.innerText).toBe("ground");
+	expect(matchedText[1][0]).toBe("<radical>coat rack</radical>");
+	expect(matchedText[1].groups?.innerText).toBe("coat rack");
 });
 
 it("Matches Reading highlighting in <reading> tags", () => {
 	const testString = `The partical は can sound like <reading>ha</reading>, but is also read like <reading>wa</reading>.`;
-	const matchedJa = [...testString.matchAll(WK_SUBJECT_MARKUP_MATCHERS.reading)];
-	expect(matchedJa).toHaveLength(2);
-	expect(matchedJa[0][0]).toBe("<reading>ha</reading>");
-	expect(matchedJa[0].groups?.innerText).toBe("ha");
-	expect(matchedJa[1][0]).toBe("<reading>wa</reading>");
-	expect(matchedJa[1].groups?.innerText).toBe("wa");
+	const matchedText = [...testString.matchAll(WK_SUBJECT_MARKUP_MATCHERS.reading)];
+	expect(matchedText).toHaveLength(2);
+	expect(matchedText[0][0]).toBe("<reading>ha</reading>");
+	expect(matchedText[0].groups?.innerText).toBe("ha");
+	expect(matchedText[1][0]).toBe("<reading>wa</reading>");
+	expect(matchedText[1].groups?.innerText).toBe("wa");
 });
 
 it("Matches Vocabulary highlighting in <vocabulary> tags", () => {
 	const testString = `The kanji 一 is used in the vocabulary <vocabulary>one thing</vocabulary> and <vocabulary>first floor</vocabulary>.`;
-	const matchedJa = [...testString.matchAll(WK_SUBJECT_MARKUP_MATCHERS.vocabulary)];
-	expect(matchedJa).toHaveLength(2);
-	expect(matchedJa[0][0]).toBe("<vocabulary>one thing</vocabulary>");
-	expect(matchedJa[0].groups?.innerText).toBe("one thing");
-	expect(matchedJa[1][0]).toBe("<vocabulary>first floor</vocabulary>");
-	expect(matchedJa[1].groups?.innerText).toBe("first floor");
+	const matchedText = [...testString.matchAll(WK_SUBJECT_MARKUP_MATCHERS.vocabulary)];
+	expect(matchedText).toHaveLength(2);
+	expect(matchedText[0][0]).toBe("<vocabulary>one thing</vocabulary>");
+	expect(matchedText[0].groups?.innerText).toBe("one thing");
+	expect(matchedText[1][0]).toBe("<vocabulary>first floor</vocabulary>");
+	expect(matchedText[1].groups?.innerText).toBe("first floor");
 });
