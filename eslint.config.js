a60c9a7a820325ed3bc41111f3e7a5087f5cb5fe
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-08-13 14:48:31 -0400

    Fix projectService
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -15,9 +15,7 @@ export default tseslint.config(
   {
     languageOptions: {
       parserOptions: {
-        projectService: {
-          allowDefaultProject: ["vitest.config.ts"],
-        },
+        projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
