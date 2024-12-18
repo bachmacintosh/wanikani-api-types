44346c9d07e1ebec061067f0440ed22f51acc3ae
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-12-18 14:03:05 -0500

    Tests for ApiRevision and DatableString
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

---
+++ tests/base/v20170710.test.ts
@@ -0,0 +1,21 @@
+import { parse } from "valibot";
+import { expect, describe, test } from "vitest";
+import { ApiRevision, DatableString } from "../../src/base/v20170710";
+
+describe("Revision 20170710: Base", () => {
+  test("ApiRevision", () => {
+    const apiRevision: ApiRevision = "20170710";
+    expect(() => parse(ApiRevision, apiRevision)).not.toThrow();
+  });
+  test("DatableString", () => {
+    const dateTimeUtcString = "2022-10-23T15:17:38.828455Z";
+    const dateTimeOffsetString = "2022-10-23T15:17:38.828455+09:00";
+    const validLeapYear = "2020-02-29T12:00:00.000000Z";
+    const dateIsoString = new Date().toISOString();
+
+    expect(() => parse(DatableString, dateTimeUtcString)).not.toThrow();
+    expect(() => parse(DatableString, dateTimeOffsetString)).not.toThrow();
+    expect(() => parse(DatableString, validLeapYear)).not.toThrow();
+    expect(() => parse(DatableString, dateIsoString)).not.toThrow();
+  });
+});
