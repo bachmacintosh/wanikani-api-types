88570fdea045ec2277fe569c491c974adc296d3a
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-12-18 14:07:33 -0500

    Consistent Valibot import
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- tests/base/v20170710.test.ts
+++ tests/base/v20170710.test.ts
@@ -1,11 +1,11 @@
-import { parse } from "valibot";
+import * as v from "valibot";
 import { expect, describe, test } from "vitest";
 import { ApiRevision, DatableString } from "../../src/base/v20170710";
 
 describe("Revision 20170710: Base", () => {
   test("ApiRevision", () => {
     const apiRevision: ApiRevision = "20170710";
-    expect(() => parse(ApiRevision, apiRevision)).not.toThrow();
+    expect(() => v.parse(ApiRevision, apiRevision)).not.toThrow();
   });
   test("DatableString", () => {
     const dateTimeUtcString = "2022-10-23T15:17:38.828455Z";
@@ -13,9 +13,9 @@ describe("Revision 20170710: Base", () => {
     const validLeapYear = "2020-02-29T12:00:00.000000Z";
     const dateIsoString = new Date().toISOString();
 
-    expect(() => parse(DatableString, dateTimeUtcString)).not.toThrow();
-    expect(() => parse(DatableString, dateTimeOffsetString)).not.toThrow();
-    expect(() => parse(DatableString, validLeapYear)).not.toThrow();
-    expect(() => parse(DatableString, dateIsoString)).not.toThrow();
+    expect(() => v.parse(DatableString, dateTimeUtcString)).not.toThrow();
+    expect(() => v.parse(DatableString, dateTimeOffsetString)).not.toThrow();
+    expect(() => v.parse(DatableString, validLeapYear)).not.toThrow();
+    expect(() => v.parse(DatableString, dateIsoString)).not.toThrow();
   });
 });
