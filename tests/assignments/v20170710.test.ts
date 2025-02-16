d9fbe9a1e1521480e722cd4ae378f7f613bc439c
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2025-02-15 19:16:18 -0500

    Add/Rework Assignment Tests
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- tests/assignments/v20170710.test.ts
+++ tests/assignments/v20170710.test.ts
@@ -1,80 +1,59 @@
 import * as v from "valibot";
-import { expect, describe, test } from "vitest";
-import { AssignmentParameters, AssignmentPayload } from "../../src/assignments/v20170710";
+import { expect, describe } from "vitest";
+import {
+  Assignment,
+  AssignmentCollection,
+  AssignmentData,
+  AssignmentParameters,
+  AssignmentPayload,
+} from "../../src/assignments/v20170710";
 import { DatableString } from "../../src/base/v20170710";
+import { testFor } from "../fixtures/v20170710";
+
+describe("AssignmentData", () => {
+  testFor("Real AssignmentData", ({ assignmentData }) => {
+    expect(() => v.assert(AssignmentData, assignmentData)).not.toThrow();
+  });
+});
+
+describe("Assignment", () => {
+  testFor("Real Assignment", ({ assignment }) => {
+    expect(() => v.assert(Assignment, assignment)).not.toThrow();
+  });
+});
+
+describe("AssignmentCollection", () => {
+  testFor("Real AssignmentCollection", ({ assignmentCollection }) => {
+    expect(() => v.assert(AssignmentCollection, assignmentCollection)).not.toThrow();
+  });
+});
 
 describe("AssignmentParameters", () => {
-  test("Empty AssignmentParameters", () => {
-    const params1: AssignmentParameters = {};
-    expect(() => v.parse(AssignmentParameters, params1)).not.toThrow();
+  testFor("Empty AssignmentParameters", ({ emptyParams }) => {
+    expect(() => v.assert(AssignmentParameters, emptyParams)).not.toThrow();
   });
-  test("AssignmentParameters with empty arrays", () => {
-    const params2: AssignmentParameters = {
-      ids: [],
-      levels: [],
-      srs_stages: [],
-      subject_ids: [],
-    };
-    expect(() => v.parse(AssignmentParameters, params2)).not.toThrow();
+  testFor("AssignmentParameters with empty arrays", ({ assignmentParamsWithEmptyArrays }) => {
+    expect(() => v.assert(AssignmentParameters, assignmentParamsWithEmptyArrays)).not.toThrow();
   });
-  test("AssignmentParameters with many options filled", () => {
-    const params3: AssignmentParameters = {
-      ids: [1, 2, 3],
-      page_after_id: 1,
-      page_before_id: 1,
-      burned: true,
-      hidden: false,
-      immediately_available_for_lessons: true,
-      immediately_available_for_review: false,
-      in_review: true,
-      levels: [1, 2, 3],
-      srs_stages: [1, 2, 3],
-      started: true,
-      subject_ids: [1, 2, 3],
-      subject_types: ["kana_vocabulary", "vocabulary"],
-      unlocked: true,
-    };
-    expect(() => v.parse(AssignmentParameters, params3)).not.toThrow();
+  testFor("AssignmentParameters with many options filled", ({ assignmentParamsWithManyOptions }) => {
+    expect(() => v.assert(AssignmentParameters, assignmentParamsWithManyOptions)).not.toThrow();
   });
-  test("AssignmentParameters with Date objects", () => {
-    const params4: AssignmentParameters = {
-      available_after: new Date(),
-      available_before: new Date(),
-      updated_after: new Date(),
-    };
-    expect(() => v.parse(AssignmentParameters, params4)).not.toThrow();
+  testFor("AssignmentParameters with Date objects", ({ assignmentParamsWithDates }) => {
+    expect(() => v.assert(AssignmentParameters, assignmentParamsWithDates)).not.toThrow();
   });
-  test("AssignmentParameters with DatableString properties", () => {
-    const params5: AssignmentParameters = {
-      available_after: v.parse(DatableString, new Date().toISOString()),
-      available_before: v.parse(DatableString, new Date().toISOString()),
-      updated_after: v.parse(DatableString, new Date().toISOString()),
-    };
-    expect(() => v.parse(AssignmentParameters, params5)).not.toThrow();
+  testFor("AssignmentParameters with DatableString properties", ({ assignmentParamsWithDatableStrings }) => {
+    expect(() => v.assert(AssignmentParameters, assignmentParamsWithDatableStrings)).not.toThrow();
   });
 });
 
 describe("AssignmentPayload", () => {
-  test("AssignmentPayload with empty assignment property", () => {
-    const payload1: AssignmentPayload = {
-      assignment: {},
-    };
-    expect(() => v.parse(AssignmentPayload, payload1)).not.toThrow();
+  testFor("AssignmentPayload with empty assignment property", ({ assignmentPayloadWithNoTime }) => {
+    expect(() => v.assert(AssignmentPayload, assignmentPayloadWithNoTime)).not.toThrow();
   });
-  test("AssignmentPayload with JS Date started_at property", () => {
-    const payload2: AssignmentPayload = {
-      assignment: {
-        started_at: new Date(),
-      },
-    };
-    expect(() => v.parse(AssignmentPayload, payload2)).not.toThrow();
+  testFor("AssignmentPayload with JS Date started_at property", ({ assignmentPayloadWithDate }) => {
+    expect(() => v.assert(AssignmentPayload, assignmentPayloadWithDate)).not.toThrow();
   });
-  test("AssignmentPayload with DatableString started_at property", () => {
-    const payload3: AssignmentPayload = {
-      assignment: {
-        started_at: v.parse(DatableString, "2024-12-27T15:32:23.000Z"),
-      },
-    };
-    expect(() => v.parse(AssignmentPayload, payload3)).not.toThrow();
+  testFor("AssignmentPayload with DatableString started_at property", ({ assignmentPayloadWithDatableString }) => {
+    expect(() => v.assert(AssignmentPayload, assignmentPayloadWithDatableString)).not.toThrow();
   });
 });
