4349f6e9578c2fd7540258ef560cedd38c121514
Collin Bachman <github@bachmac.com>
2025-02-24 05:33:36 -0500

    BREAKING: Remove Individual Subject Resources (#396)
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- tests/subjects/v20170710.test-d.ts
+++ tests/subjects/v20170710.test-d.ts
@@ -1,56 +1,36 @@
-import type {
-  KanaVocabulary,
-  KanaVocabularyCollection,
-  Kanji,
-  KanjiCollection,
-  Radical,
-  RadicalCollection,
-  Subject,
-  SubjectCollection,
-  SubjectParameters,
-  Vocabulary,
-  VocabularyCollection,
-} from "../../src/subjects/v20170710.js";
+import type { Subject, SubjectCollection, SubjectParameters } from "../../src/subjects/v20170710.js";
 import { assertType, describe } from "vitest";
 import { testFor } from "../fixtures/v20170710.js";
 
 describe("Subjects", () => {
   testFor("Real Radical", ({ radical }) => {
-    assertType<Radical>(radical);
     assertType<Subject>(radical);
   });
   testFor("Real Kanji", ({ kanji }) => {
-    assertType<Kanji>(kanji);
     assertType<Subject>(kanji);
   });
   testFor("Real Vocabulary", ({ vocabulary }) => {
-    assertType<Vocabulary>(vocabulary);
     assertType<Subject>(vocabulary);
   });
-  testFor("Real KanaVocabulary", ({ kanaVocabulary }) => {
-    assertType<KanaVocabulary>(kanaVocabulary);
+  testFor("Real Kana-Only Vocabulary", ({ kanaVocabulary }) => {
     assertType<Subject>(kanaVocabulary);
   });
 });
 
 describe("Subject Collections", () => {
-  testFor("Real RadicalCollection", ({ radicalCollection }) => {
-    assertType<RadicalCollection>(radicalCollection);
+  testFor("Collection of Radicals", ({ radicalCollection }) => {
     assertType<SubjectCollection>(radicalCollection);
   });
-  testFor("Real KanjiCollection", ({ kanjiCollection }) => {
-    assertType<KanjiCollection>(kanjiCollection);
+  testFor("Collection of Kanji", ({ kanjiCollection }) => {
     assertType<SubjectCollection>(kanjiCollection);
   });
-  testFor("Real VocabularyCollection", ({ vocabularyCollection }) => {
-    assertType<VocabularyCollection>(vocabularyCollection);
+  testFor("Collection of Vocabulary", ({ vocabularyCollection }) => {
     assertType<SubjectCollection>(vocabularyCollection);
   });
-  testFor("Real KanaVocabularyCollection", ({ kanaVocabularyCollection }) => {
-    assertType<KanaVocabularyCollection>(kanaVocabularyCollection);
+  testFor("Collection of Kana-Only Vocabulary", ({ kanaVocabularyCollection }) => {
     assertType<SubjectCollection>(kanaVocabularyCollection);
   });
-  testFor("Real SubjectCollection", ({ subjectCollection }) => {
+  testFor("Collection of Mixed Subjects", ({ subjectCollection }) => {
     assertType<SubjectCollection>(subjectCollection);
   });
 });
