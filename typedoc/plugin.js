ae87fa9835e70c3d6e5fa000562608c0ff718b27
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2025-03-10 12:49:31 -0400

    Docs: Add Cloudflare Router
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- typedoc/plugin.js
+++ typedoc/plugin.js
@@ -1,11 +1,38 @@
 // @ts-check
-import { Converter, ReflectionKind } from "typedoc";
-/** @typedef { import("typedoc").Application } Application */
+import { Application, Converter, KindRouter, ReflectionKind } from "typedoc";
+
+class CloudflareRouter extends KindRouter {
+  /** @type Map<ReflectionKind, string> */
+  directories = new Map([
+    [ReflectionKind.Class, "classes"],
+    [ReflectionKind.Interface, "interfaces"],
+    [ReflectionKind.Enum, "enums"],
+    [ReflectionKind.Namespace, "modules"],
+    [ReflectionKind.Module, "modules"],
+    [ReflectionKind.TypeAlias, "types"],
+    [ReflectionKind.Function, "methods"],
+    [ReflectionKind.Variable, "variables"],
+    [ReflectionKind.Document, "documents"],
+  ]);
+
+  /**
+   *
+   * @param {Application} app
+   */
+  constructor(app) {
+    super(app);
+    this.application.logger.info("Using Cloudflare Router");
+  }
+}
 
 /**
  * @param {Application} app
  */
 export function load(app) {
+  app.on(Application.EVENT_BOOTSTRAP_END, () => {
+    app.renderer.defineRouter("cloudflare", CloudflareRouter);
+  });
+
   app.converter.on(Converter.EVENT_CREATE_DECLARATION, (context, refl) => {
     // Remove any Valibot schemas from the documentation
     if (
