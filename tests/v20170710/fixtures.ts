a7cdbae9778dc8de20e8be8c6b58aaf85abc626b
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2025-03-14 14:17:23 -0400

    Add/Update tests
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- tests/v20170710/fixtures.ts
+++ tests/v20170710/fixtures.ts
@@ -998,7 +998,11 @@ export const testFor = test.extend({
   subjectMarkupWithReadingTag: `The partical は can sound like <reading>ha</reading>, but is also read like <reading>wa</reading>.`,
   subjectMarkupWithVocabularyTag: `The kanji 一 is used in the vocabulary <vocabulary>one thing</vocabulary> and <vocabulary>first floor</vocabulary>.`,
   subjectMarkupWithRadicalAndKanjiTags: `The kanji <kanji>three</kanji> is made up of the <radical>one</radical> and <radical>two</radical> radicals.`,
-  subjectMarkupWithKanjiJaAndReadingTags: `The <kanji>mud</kanji> kanji is read like <reading>do-ro</reading> (<ja>どろ</ja>).`,
+  subjectMarkupWithKanjiJaAndReadingTags: `The <kanji>mud</kanji> kanji is read like <reading>doro</reading> (<ja>どろ</ja>).`,
+  subjectMarkupWithKanjiAndVocabularyTags: `The vocabulary <vocabulary>oil painting</vocabulary> uses the <kanji>oil</kanji> and <kanji>drawing</kanji> kanji.`,
+  subjectMarkupWithVocabularyJaReadingAndMeaningTags: `The vocabulary <vocabulary>girl</vocabulary> can mean <meaning>maiden</meaning> as well, and is read <reading>shoujo</reading> (<ja>しょうじょ</ja>).`,
+  subjectMarkupWithNestedTags: `The vocabulary <vocabulary>to go up</vocabulary> has a nested <ja><reading>word</reading></ja> in it. It's not <ja>very <reading>common</reading> to</ja> see this, but we should test for it.`,
+  subjectMarkupWithEmptyTag: `Oops, this <ja></ja> tag has no text in it.`,
 
   // Summary
   summary: {
